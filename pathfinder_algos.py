db8e2a0f07306803af6fa6d0a6b240500f0c1be1
ShanaryS <shanaryjs@gmail.com>
2021-09-02 02:34:13 -0400

    Cleaned up code

--- pathfinder_algos.py
+++ pathfinder_algos.py
@@ -77,7 +77,6 @@ def dijkstra(maze, start):
                 adj_vertex.pred_vertex = current_vertex
 
 
-# Make own a_star from scratch using graph.py
 # def _a_star(graph, start, stop):
 #     open_lst = {[start]}
 #     closed_lst = set([])
@@ -144,16 +143,13 @@ def a_star(maze, start, end, allow_diagonal_movement=False):
     heapq.heapify(open_list)
     heapq.heappush(open_list, start_node)
 
-    # Adding a stop condition
     outer_iterations = 0
     max_iterations = (len(maze[0]) * len(maze) // 2)
 
-    # what squares do we search
     adjacent_squares = ((0, -1), (0, 1), (-1, 0), (1, 0),)
     if allow_diagonal_movement:
         adjacent_squares = ((0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1),)
 
-    # Loop until you find the end
     while len(open_list) > 0:
         outer_iterations += 1
 
@@ -163,55 +159,42 @@ def a_star(maze, start, end, allow_diagonal_movement=False):
             warn("giving up on pathfinding too many iterations")
             return return_path(current_node)
 
-            # Get the current node
         current_node = heapq.heappop(open_list)
         closed_list.append(current_node)
 
-        # Found the goal
         if current_node == end_node:
             return return_path(current_node)
 
-        # Generate children
         children = []
 
-        for new_position in adjacent_squares:  # Adjacent squares
+        for new_position in adjacent_squares:
 
-            # Get node position
             node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
 
-            # Make sure within range
             if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (
                     len(maze[len(maze) - 1]) - 1) or node_position[1] < 0:
                 continue
 
-            # Make sure walkable terrain
             if maze[node_position[0]][node_position[1]] != 0:
                 continue
 
-            # Create new node
             new_node = Node(current_node, node_position)
 
-            # Append
             children.append(new_node)
 
-        # Loop through children
         for child in children:
-            # Child is on the closed list
             if len([closed_child for closed_child in closed_list if closed_child == child]) > 0:
                 continue
 
-            # Create the f, g, and h values
             child.g = current_node.g + 1
             child.h = ((child.position[0] - end_node.position[0]) ** 2) + (
                         (child.position[1] - end_node.position[1]) ** 2)
             child.f = child.g + child.h
 
-            # Child is already in the open list
             if len([open_node for open_node in open_list if
                     child.position == open_node.position and child.g > open_node.g]) > 0:
                 continue
 
-            # Add the child to the open list
             heapq.heappush(open_list, child)
 
     warn("Couldn't get a path to destination")
@@ -224,4 +207,4 @@ def return_path(current_node):
     while current is not None:
         path.append(current.position)
         current = current.parent
-    return path[::-1]  # Return reversed path
+    return path[::-1]
