0de2c171504ca2b1ebca70eefd68323e9aae6501
ShanaryS <shanaryjs@gmail.com>
2021-09-10 03:00:19 -0400

    Reorganized file structure

--- calc/colors.py
+++ calc/colors.py
@@ -1,6 +1,20 @@
 """Colors used for visualizations"""
 
-# Pygame colors
+
+# Matplotlib colors (Search and Sort)
+MPL_DEFAULT = 'blue'
+MPL_GOLD = 'gold'          # Checking
+MPL_MAGENTA = 'magenta'    # Pivot
+MPL_CYAN = 'cyan'          # Special value unique to that algorithm
+MPL_GREEN = 'green'        # Result
+MPL_RED = 'red'            # Checked but false
+MPL_BLACK = 'black'        # Misc
+MPL_ORANGE = 'orange'      # Used for buttons
+MPL_YELLOW = 'yellow'      # Used for buttons
+MPL_TOMATO = 'tomato'      # Used for buttons
+
+
+# Pygame colors (Pathfinding)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GREY = (128, 128, 128)
@@ -14,15 +28,17 @@ TURQUOISE = (64, 224, 208)
 TURQUOISE_ALT = (64, 223, 208)
 TURQUOISE_ALT_ = (64, 225, 208)
 
-
-# Matplotlib colors
-MPL_DEFAULT = 'blue'
-MPL_GOLD = 'gold'          # Checking
-MPL_MAGENTA = 'magenta'    # Pivot
-MPL_CYAN = 'cyan'          # Special value unique to that algorithm
-MPL_GREEN = 'green'        # Result
-MPL_RED = 'red'            # Checked but false
-MPL_BLACK = 'black'        # Misc
-MPL_ORANGE = 'orange'      # Used for buttons
-MPL_YELLOW = 'yellow'      # Used for buttons
-MPL_TOMATO = 'tomato'      # Used for buttons
+DEFAULT_COLOR = WHITE
+LEGEND_AREA_COLOR = GREY
+LINE_COLOR = GREY
+OPEN_COLOR = TURQUOISE
+OPEN_ALT_COLOR = TURQUOISE_ALT
+OPEN_ALT_COLOR_ = TURQUOISE_ALT_
+CLOSED_COLOR = BLUE
+START_COLOR = GREEN
+MID_COLOR = ORANGE
+END_COLOR = RED
+WALL_COLOR = BLACK
+PATH_COLOR = YELLOW
+LEGEND_COLOR = BLACK
+VIS_COLOR = RED
--- graph/node.py
+++ graph/node.py
@@ -1,10 +1,10 @@
+from calc.colors import *
+
+
 class Square:
     """Defines the properties needed for each node on graph"""
-    def __init__(self, row, col):
-        # Gains access to parent class methods and attributes
-        super().__init__()
 
-        # Defines attributes of nodes
+    def __init__(self, row, col):
         self.rows = self.rows
         self.square_size = self.square_size
         self.row = row
@@ -12,7 +12,7 @@ class Square:
         self.x = self.row * self.square_size
         self.y = self.col * self.square_size
         self.neighbours = []
-        self.color = self.DEFAULT_COLOR
+        self.color = MPL_DEFAULT
 
     def __lt__(self, other):
         """Allows comparison of squares"""
@@ -36,85 +36,85 @@ class Square:
 
     def is_empty(self):
         """Checks if blank node"""
-        return self.color == self.DEFAULT_COLOR
+        return self.color == DEFAULT_COLOR
 
     def is_open(self):
         """Checks if open node"""
-        return self.color == self.OPEN_COLOR
+        return self.color == OPEN_COLOR
 
     def is_open_alt(self):
         """Checks if open node for second swarm of bi_dijkstra"""
-        return self.color == self.OPEN_ALT_COLOR
+        return self.color == OPEN_ALT_COLOR
 
     def is_open_alt_(self):
         """Checks if open node for end node when mid is included"""
-        return self.color == self.OPEN_ALT_COLOR_
+        return self.color == OPEN_ALT_COLOR_
 
     def is_closed(self):
         """Checks if closed node"""
-        return self.color == self.CLOSED_COLOR
+        return self.color == CLOSED_COLOR
 
     def is_start(self):
         """Checks if start node"""
-        return self.color == self.START_COLOR
+        return self.color == START_COLOR
 
     def is_mid(self):
         """Checks if mid node"""
-        return self.color == self.MID_COLOR
+        return self.color == MID_COLOR
 
     def is_end(self):
         """Checks if end node"""
-        return self.color == self.END_COLOR
+        return self.color == END_COLOR
 
     def is_wall(self):
         """Checks if wall node"""
-        return self.color == self.WALL_COLOR
+        return self.color == WALL_COLOR
 
     def is_path(self):
         """Checks if path node"""
-        return self.color == self.PATH_COLOR
+        return self.color == PATH_COLOR
 
     def reset(self):
         """Sets node to blank"""
-        self.color = self.DEFAULT_COLOR
+        self.color = DEFAULT_COLOR
 
     def set_open(self):
         """Sets node to open"""
-        self.color = self.OPEN_COLOR
+        self.color = OPEN_COLOR
 
     def set_open_alt(self):
         """Sets node to open for second swarm of bi_dijkstra"""
-        self.color = self.OPEN_ALT_COLOR
+        self.color = OPEN_ALT_COLOR
 
     def set_open_alt_(self):
         """Sets node to open for end node when mid is included.
         Each swarms needs to be different colors for best path algo to work.
         """
-        self.color = self.OPEN_ALT_COLOR_
+        self.color = OPEN_ALT_COLOR_
 
     def set_closed(self):
         """Sets node to closed"""
-        self.color = self.CLOSED_COLOR
+        self.color = CLOSED_COLOR
 
     def set_start(self):
         """Sets node to start"""
-        self.color = self.START_COLOR
+        self.color = START_COLOR
 
     def set_mid(self):
         """Sets node to mid"""
-        self.color = self.MID_COLOR
+        self.color = MID_COLOR
 
     def set_end(self):
         """Sets node to end"""
-        self.color = self.END_COLOR
+        self.color = END_COLOR
 
     def set_wall(self):
         """Sets node to wall"""
-        self.color = self.WALL_COLOR
+        self.color = WALL_COLOR
 
     def set_path(self):
         """Sets node to path"""
-        self.color = self.PATH_COLOR
+        self.color = PATH_COLOR
 
     def draw_square(self):
         """Updates the square with node type"""
