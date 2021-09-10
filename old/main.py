297dcdd0510129adf38232c4cf35739157367919
ShanaryS <shanaryjs@gmail.com>
2021-09-10 14:00:45 -0400

    Reorganized file structure

--- old/main.py
+++ old/main.py
@@ -1,5 +1,6 @@
 """Old code for visualizers. Still works and almost feature complete.
 Run visualizers by uncommenting code. Must be '__main__'.
+Pygame is slow to load and change size. Only exists for this old version. Fixed in the current version.
 """
 
 
@@ -15,7 +16,6 @@ if __name__ == '__main__':
     # Sort.set_graph()
 
     '''Uncomment these to run Search Visualizer. Keep others as comments.'''
-    from vis_path import PathfindingVisualizer
-    Path = PathfindingVisualizer()
-    Path.main()
-    # Pygame is slow to load and change size. Only exists for this old version. Fixed in the current version.
+    # from vis_path import PathfindingVisualizer
+    # Path = PathfindingVisualizer()
+    # Path.main()
