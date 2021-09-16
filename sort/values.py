6a35757bffd3c5b180ddeacb53d3fc4fc6823852
ShanaryS <shanaryjs@gmail.com>
2021-09-16 19:47:47 -0400

    Reorganized file structure

--- sort/values.py
+++ sort/values.py
@@ -3,16 +3,16 @@ import numpy as np
 from collections import OrderedDict
 
 
-def generate_array(low, high, size=None):
+def generate_array(low: int, high: int, size: int = None) -> np.ndarray:
     """Returns generated numbers to be used for search and sort visualizations"""
     return np.random.randint(low, high, size)
 
 
-def remove_duplicates(array):
+def remove_duplicates(array: np.ndarray) -> list[np.ndarray]:
     """Return list of array with no duplicates. Preserves order. Used by radix sort."""
     return list(OrderedDict.fromkeys(array))
 
 
-def get_factorial(num):
+def get_factorial(num: int) -> int:
     """Returns factorial of a number. Used by bogosort"""
     return np.math.factorial(num)
