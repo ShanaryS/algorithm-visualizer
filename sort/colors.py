67fbe029fd1f05ae7a9e9a4b88a73095df06b06e
ShanaryS <shanaryjs@gmail.com>
2021-09-16 19:48:11 -0400

    Reorganized file structure

--- sort/colors.py
+++ sort/colors.py
@@ -2,13 +2,13 @@
 
 
 # Matplotlib colors
-MPL_DEFAULT = 'blue'
-MPL_GOLD = 'gold'          # Checking
-MPL_MAGENTA = 'magenta'    # Pivot
-MPL_CYAN = 'cyan'          # Special value unique to that algorithm
-MPL_GREEN = 'green'        # Result
-MPL_BLACK = 'black'        # To swap
-MPL_RED = 'red'            # Moving
-MPL_ORANGE = 'orange'      # Used for buttons
-MPL_YELLOW = 'yellow'      # Used for buttons
-MPL_TOMATO = 'tomato'      # Used for buttons
+MPL_DEFAULT: str = 'blue'
+MPL_GOLD: str = 'gold'          # Checking
+MPL_MAGENTA: str = 'magenta'    # Pivot
+MPL_CYAN: str = 'cyan'          # Special value unique to that algorithm
+MPL_GREEN: str = 'green'        # Result
+MPL_BLACK: str = 'black'        # To swap
+MPL_RED: str = 'red'            # Moving
+MPL_ORANGE: str = 'orange'      # Used for buttons
+MPL_YELLOW: str = 'yellow'      # Used for buttons
+MPL_TOMATO: str = 'tomato'      # Used for buttons
