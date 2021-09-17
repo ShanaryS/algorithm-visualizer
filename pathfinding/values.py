e46e99f536c29644b160389ddd52982ad9954bb0
ShanaryS <shanaryjs@gmail.com>
2021-09-16 20:37:07 -0400

    Reorganized file structure

--- pathfinding/values.py
+++ pathfinding/values.py
@@ -6,16 +6,16 @@ import random
 
 # Default counts for pathfinding graph. Defining here so that both logic.py and node.py has access.
 # Recursive division only works on certain row values. 22,23,46,47,94,95.
-ROWS = 46
-WIDTH_HEIGHT = 800
-SQUARE_SIZE = WIDTH_HEIGHT / ROWS
+ROWS: int = 46
+WIDTH_HEIGHT: int = 800
+SQUARE_SIZE: float = WIDTH_HEIGHT / ROWS
 
 
-def get_random_sample(population, k) -> list:
+def get_random_sample(population: list, k: int) -> list:
     """Returns a k length list of unique elements from population"""
     return random.sample(population, k)
 
 
-def get_randrange(start, stop) -> int:
+def get_randrange(start: int, stop: int) -> int:
     """Return a random int within a range"""
     return random.randrange(start, stop)
