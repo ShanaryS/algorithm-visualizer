ff4cdb1c65ec37b2d1e26885c882feb48c5e2913
ShanaryS <shanaryjs@gmail.com>
2021-09-27 20:26:01 -0400

    Cleaned up code

--- search/colors.py
+++ search/colors.py
@@ -4,8 +4,8 @@ from enum import Enum, unique
 
 
 @unique
-class MatplotlibColors(Enum):
-    """Colors for matplotlib"""
+class Colors(Enum):
+    """Name for certain colors"""
 
     BLUE = 'blue'
     GOLD = 'gold'
@@ -17,11 +17,14 @@ class MatplotlibColors(Enum):
     YELLOW = 'yellow'
 
 
-MPL_DEFAULT = MatplotlibColors.BLUE
-MPL_GOLD = MatplotlibColors.GOLD          # Checking
-MPL_MAGENTA = MatplotlibColors.MAGENTA    # Pivot
-MPL_CYAN = MatplotlibColors.CYAN          # Special value unique to that algorithm
-MPL_GREEN = MatplotlibColors.GREEN        # Result
-MPL_RED = MatplotlibColors.RED            # Checked but false
-MPL_BLACK = MatplotlibColors.BLACK        # Misc
-MPL_YELLOW = MatplotlibColors.YELLOW      # Used for buttons
+class MatplotlibColors(Enum):
+    """Matplotlib colors"""
+
+    MPL_DEFAULT = Colors.BLUE
+    MPL_GOLD = Colors.GOLD          # Checking
+    MPL_MAGENTA = Colors.MAGENTA    # Pivot
+    MPL_CYAN = Colors.CYAN          # Special value unique to that algorithm
+    MPL_GREEN = Colors.GREEN        # Result
+    MPL_RED = Colors.RED            # Checked but false
+    MPL_BLACK = Colors.BLACK        # Misc
+    MPL_YELLOW = Colors.YELLOW      # Used for buttons
--- sort/colors.py
+++ sort/colors.py
@@ -4,8 +4,8 @@ from enum import Enum, unique
 
 
 @unique
-class MatplotlibColors(Enum):
-    """Colors for matplotlib"""
+class Colors(Enum):
+    """Name for certain colors"""
 
     BLUE = 'blue'
     GOLD = 'gold'
@@ -19,13 +19,16 @@ class MatplotlibColors(Enum):
     TOMATO = 'tomato'
 
 
-MPL_DEFAULT = MatplotlibColors.BLUE
-MPL_GOLD = MatplotlibColors.GOLD          # Checking
-MPL_MAGENTA = MatplotlibColors.MAGENTA    # Pivot
-MPL_CYAN = MatplotlibColors.CYAN          # Special value unique to that algorithm
-MPL_GREEN = MatplotlibColors.GREEN        # Result
-MPL_BLACK = MatplotlibColors.BLACK        # To swap
-MPL_RED = MatplotlibColors.RED            # Moving
-MPL_ORANGE = MatplotlibColors.ORANGE      # Used for buttons
-MPL_YELLOW = MatplotlibColors.YELLOW      # Used for buttons
-MPL_TOMATO = MatplotlibColors.TOMATO      # Used for buttons
+class MatplotlibColors(Enum):
+    """Colors for matplotlib"""
+
+    MPL_DEFAULT = Colors.BLUE
+    MPL_GOLD = Colors.GOLD          # Checking
+    MPL_MAGENTA = Colors.MAGENTA    # Pivot
+    MPL_CYAN = Colors.CYAN          # Special value unique to that algorithm
+    MPL_GREEN = Colors.GREEN        # Result
+    MPL_BLACK = Colors.BLACK        # To swap
+    MPL_RED = Colors.RED            # Moving
+    MPL_ORANGE = Colors.ORANGE      # Used for buttons
+    MPL_YELLOW = Colors.YELLOW      # Used for buttons
+    MPL_TOMATO = Colors.TOMATO      # Used for buttons
