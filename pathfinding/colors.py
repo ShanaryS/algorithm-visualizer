9b3e353c44a305e6e7a56b2e0eebad803100eed0
ShanaryS <shanaryjs@gmail.com>
2021-09-27 20:23:25 -0400

    Cleaned up code

--- pathfinding/colors.py
+++ pathfinding/colors.py
@@ -1,31 +1,40 @@
 """Colors needed for visualization"""
 
+from enum import Enum, unique
 
-# Pygame colors
-WHITE: tuple = (255, 255, 255)
-BLACK: tuple = (0, 0, 0)
-GREY: tuple = (128, 128, 128)
-RED: tuple = (255, 0, 0)
-GREEN: tuple = (0, 255, 0)
-BLUE: tuple = (0, 0, 255)
-YELLOW: tuple = (255, 255, 0)
-ORANGE: tuple = (255, 165, 0)
-PURPLE: tuple = (128, 0, 128)
-TURQUOISE: tuple = (64, 224, 208)
-TURQUOISE_ALT: tuple = (64, 223, 208)
-TURQUOISE_ALT_: tuple = (64, 225, 208)
 
-DEFAULT_COLOR: tuple = WHITE
-LEGEND_AREA_COLOR: tuple = GREY
-LINE_COLOR: tuple = GREY
-OPEN_COLOR: tuple = TURQUOISE
-OPEN_ALT_COLOR: tuple = TURQUOISE_ALT
-OPEN_ALT_COLOR_: tuple = TURQUOISE_ALT_
-CLOSED_COLOR: tuple = BLUE
-START_COLOR: tuple = GREEN
-MID_COLOR: tuple = ORANGE
-END_COLOR: tuple = RED
-WALL_COLOR: tuple = BLACK
-PATH_COLOR: tuple = YELLOW
-LEGEND_COLOR: tuple = BLACK
-VIS_COLOR: tuple = RED
+@unique
+class Colors(Enum):
+    """RGB values of colors"""
+
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    GREY = (128, 128, 128)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    YELLOW = (255, 255, 0)
+    ORANGE = (255, 165, 0)
+    PURPLE = (128, 0, 128)
+    TURQUOISE = (64, 224, 208)
+    TURQUOISE_ALT = (64, 223, 208)
+    TURQUOISE_ALT_ = (64, 225, 208)
+
+
+class PygameColors(Enum):
+    """Pygame colors"""
+
+    DEFAULT_COLOR = Colors.WHITE
+    LEGEND_AREA_COLOR = Colors.GREY
+    LINE_COLOR = Colors.GREY
+    OPEN_COLOR = Colors.TURQUOISE
+    OPEN_ALT_COLOR = Colors.TURQUOISE_ALT
+    OPEN_ALT_COLOR_ = Colors.TURQUOISE_ALT_
+    CLOSED_COLOR = Colors.BLUE
+    START_COLOR = Colors.GREEN
+    MID_COLOR = Colors.ORANGE
+    END_COLOR = Colors.RED
+    WALL_COLOR = Colors.BLACK
+    PATH_COLOR = Colors.YELLOW
+    LEGEND_COLOR = Colors.BLACK
+    VIS_COLOR = Colors.RED
