0a3ba894443abe356a2a35e15ab5845828ff2dd6
ShanaryS <shanaryjs@gmail.com>
2021-09-27 20:15:19 -0400

    Cleaned up code

--- search/colors.py
+++ search/colors.py
@@ -1,12 +1,27 @@
 """Colors needed for visualizations"""
 
+from enum import Enum, unique
 
-# Matplotlib colors
-MPL_DEFAULT: str = 'blue'
-MPL_GOLD: str = 'gold'          # Checking
-MPL_MAGENTA: str = 'magenta'    # Pivot
-MPL_CYAN: str = 'cyan'          # Special value unique to that algorithm
-MPL_GREEN: str = 'green'        # Result
-MPL_RED: str = 'red'            # Checked but false
-MPL_BLACK: str = 'black'        # Misc
-MPL_YELLOW: str = 'yellow'      # Used for buttons
+
+@unique
+class MatplotlibColors(Enum):
+    """Colors for matplotlib"""
+
+    BLUE: str = 'blue'
+    GOLD: str = 'gold'
+    MAGENTA: str = 'magenta'
+    CYAN: str = 'cyan'
+    GREEN: str = 'green'
+    RED: str = 'red'
+    BLACK: str = 'black'
+    YELLOW: str = 'yellow'
+
+
+MPL_DEFAULT = MatplotlibColors.BLUE
+MPL_GOLD = MatplotlibColors.GOLD          # Checking
+MPL_MAGENTA = MatplotlibColors.MAGENTA    # Pivot
+MPL_CYAN = MatplotlibColors.CYAN          # Special value unique to that algorithm
+MPL_GREEN = MatplotlibColors.GREEN        # Result
+MPL_RED = MatplotlibColors.RED            # Checked but false
+MPL_BLACK = MatplotlibColors.BLACK        # Misc
+MPL_YELLOW = MatplotlibColors.YELLOW      # Used for buttons
