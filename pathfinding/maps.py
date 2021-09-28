933a4494befa94ecae84399e000ee5f37ed6bfdc
ShanaryS <shanaryjs@gmail.com>
2021-09-28 04:13:34 -0400

    Added ability to get images using google maps api

--- pathfinding/maps.py
+++ pathfinding/maps.py
@@ -1,3 +1,30 @@
+import os
 import requests
 from dotenv import load_dotenv
 
+
+def get_api_key() -> str:
+    """Gets api key from local environmental variables"""
+    load_dotenv()
+    API_KEY = os.getenv('API_KEY')
+    return f'&key={API_KEY}'
+
+
+def get_img() -> bytes:
+    """Gets the images from the specified url"""
+
+    base_url = 'https://maps.googleapis.com/maps/api/staticmap?'
+    params = 'center=Berkeley,CA&zoom=14&size=400x400'
+    KEY = get_api_key()
+    url = base_url + params + KEY
+
+    img = requests.get(url).content
+
+    return img
+
+
+def write_img(img) -> None:
+    """Saves image as file"""
+
+    with open('img.jpg', 'wb') as img_file:
+        img_file.write(img)
