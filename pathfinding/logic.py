648dc3f98d4b59f5543781b229bda7a8f3cf464e
ShanaryS <shanaryjs@gmail.com>
2021-09-29 13:05:17 -0400

    Map feature completed for single address

--- pathfinding/logic.py
+++ pathfinding/logic.py
@@ -414,20 +414,3 @@ def _get_address_from_user(gph: GraphState, algo: AlgoState, lgc: LogicState, tx
         draw_vis_text(txt, is_input=True)
 
     pygame.quit()
-
-
-'''
-New features for the future:
-
-Instantly update algo when draw wall after completion, much like dragging nodes
-Add prim maze and sticky mud
-
-
-Bugs to fix:
-
-When clicking to remove start/end node with mid node and reinstating it on completed algo, doesn't update properly
-Bi-directional dijkstra only draws best_path when edges of swarms are touching. Only manifests with mid nodes
-Maze can change size if window loses focus for a few seconds. Mainly with the large maze.
-    pygame.event.set_grab prevents mouse from leaving window but also prevents exists
-    pygame.mouse.get_focused() potential elegant solution
-'''
