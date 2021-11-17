0979e47d8e70243c8c555dfcb1b776386a252716
Shanary <86130442+ShanaryS@users.noreply.github.com>
2021-11-17 12:37:42 -0500

    Update create_exe.bat

--- create_exe.bat
+++ create_exe.bat
@@ -3,7 +3,7 @@
 set name="Pathfinding Visualizer"
 set src="run_pathfinding_visualizer.py"
 
-venv\Scripts\pyinstaller.exe -F -n %name% --distpath ./bin -p "venv\Lib\site-packages" --noconsole %src%
+venv\Scripts\pyinstaller.exe -F -n %name% -p "venv\Lib\site-packages" --noconsole --distpath ./bin %src%
 
 del %name%".spec"
 rmdir /s /q "__pycache__" "build"
@@ -12,7 +12,7 @@ rmdir /s /q "__pycache__" "build"
 set name="Sorting Visualizer"
 set src="run_sort_visualizer.py"
 
-venv\Scripts\pyinstaller.exe -F -n %name% --distpath ./bin -p "venv\Lib\site-packages" --noconsole %src%
+venv\Scripts\pyinstaller.exe -F -n %name% -p "venv\Lib\site-packages" --noconsole --distpath ./bin %src%
 
 del %name%".spec"
 rmdir /s /q "__pycache__" "build"
@@ -21,7 +21,7 @@ rmdir /s /q "__pycache__" "build"
 set name="Searching Visualizer"
 set src="run_search_visualizer.py"
 
-venv\Scripts\pyinstaller.exe -F -n %name% --distpath ./bin -p "venv\Lib\site-packages" --noconsole %src%
+venv\Scripts\pyinstaller.exe -F -n %name% -p "venv\Lib\site-packages" --noconsole --distpath ./bin %src%
 
 del %name%".spec"
 rmdir /s /q "__pycache__" "build"
