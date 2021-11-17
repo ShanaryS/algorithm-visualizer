e48b161d438b00e9f656d4f58879b08cafa52f35
Shanary <86130442+ShanaryS@users.noreply.github.com>
2021-11-17 12:22:00 -0500

    Update create_exe.bat

--- create_exe.bat
+++ create_exe.bat
@@ -1,20 +1,34 @@
-set name="Pathfinding Visualizer"
+@echo off
 
-venv\Scripts\pyinstaller.exe -F -n %name% --distpath ./bin -p "venv\Lib\site-packages" --noconsole run_pathfinding_visualizer.py
+set name="Pathfinding Visualizer"
+set src="run_pathfinding_visualizer.py"
+
+venv\Scripts\pyinstaller.exe -F -n %name% --distpath ./bin -p "venv\Lib\site-packages" --noconsole %src%
 
 del %name%".spec"
 rmdir /s /q "__pycache__" "build"
+
 
 set name="Sorting Visualizer"
+set src="run_sort_visualizer.py"
 
-venv\Scripts\pyinstaller.exe -F -n %name% --distpath ./bin -p "venv\Lib\site-packages" --noconsole run_sort_visualizer.py
+venv\Scripts\pyinstaller.exe -F -n %name% --distpath ./bin -p "venv\Lib\site-packages" --noconsole %src%
 
 del %name%".spec"
 rmdir /s /q "__pycache__" "build"
+
 
 set name="Searching Visualizer"
+set src="run_search_visualizer.py"
 
-venv\Scripts\pyinstaller.exe -F -n %name% --distpath ./bin -p "venv\Lib\site-packages" --noconsole run_search_visualizer.py
+venv\Scripts\pyinstaller.exe -F -n %name% --distpath ./bin -p "venv\Lib\site-packages" --noconsole %src%
 
 del %name%".spec"
 rmdir /s /q "__pycache__" "build"
+
+
+echo.
+echo --------------------------------------------------------------
+echo Executable and source files located in "bin" directory.
+echo.
+pause
