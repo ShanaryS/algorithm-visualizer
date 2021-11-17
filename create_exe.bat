c47625c2931d9a7c5161632a82c69ee88923c394
ShanaryS <shanaryjs@gmail.com>
2021-11-16 19:22:49 -0500

    pyinstaller

--- create_exe.bat
+++ create_exe.bat
@@ -1,20 +1,20 @@
 set name="Pathfinding Visualizer"
 
-venv\Scripts\pyinstaller.exe -F -n %name% --distpath ./bin -p ".venv\Lib\site-packages" --noconsole run_pathfinding_visualizer.py
+venv\Scripts\pyinstaller.exe -F -n %name% --distpath ./bin -p "venv\Lib\site-packages" --noconsole run_pathfinding_visualizer.py
 
 del %name%".spec"
 rmdir /s /q "__pycache__" "build"
 
 set name="Sorting Visualizer"
 
-venv\Scripts\pyinstaller.exe -F -n %name% --distpath ./bin -p ".venv\Lib\site-packages" --noconsole --noconsole run_sort_visualizer.py
+venv\Scripts\pyinstaller.exe -F -n %name% --distpath ./bin -p "venv\Lib\site-packages" --noconsole --noconsole run_sort_visualizer.py
 
 del %name%".spec"
 rmdir /s /q "__pycache__" "build"
 
 set name="Searching Visualizer"
 
-venv\Scripts\pyinstaller.exe -F -n %name% --distpath ./bin -p ".venv\Lib\site-packages" --noconsole --noconsole run_search_visualizer.py
+venv\Scripts\pyinstaller.exe -F -n %name% --distpath ./bin -p "venv\Lib\site-packages" --noconsole --noconsole run_search_visualizer.py
 
 del %name%".spec"
 rmdir /s /q "__pycache__" "build"
