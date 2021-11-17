a81b956a8b90588e6995f29f55bc4112a1d1efa8
ShanaryS <shanaryjs@gmail.com>
2021-11-17 17:34:48 -0500

    exe fix

--- run_pathfinding_visualizer.py
+++ run_pathfinding_visualizer.py
@@ -9,6 +9,24 @@ import os
 
 def override_where():
     """Overrides certifi.core.where to return actual location of cacert.pem"""
+    # Fixes error when compiling program to exe.
+    if hasattr(sys, "frozen"):
+        import certifi.core
+
+        os.environ["REQUESTS_CA_BUNDLE"] = _override_where()
+        certifi.core.where = _override_where
+
+        # delay importing until after where() has been replaced
+        import requests.utils
+        import requests.adapters
+        # replace these variables in case these modules were
+        # imported before we replaced certifi.core.where
+        requests.utils.DEFAULT_CA_BUNDLE_PATH = _override_where()
+        requests.adapters.DEFAULT_CA_BUNDLE_PATH = _override_where()
+
+
+def _override_where():
+    """Helper function"""
     # change this to match the location of cacert.pem
     return os.path.abspath(
         os.path.join('venv', 'Lib', 'site-packages', 'certifi', 'cacert.pem'))
@@ -16,6 +34,8 @@ def override_where():
 
 def main() -> None:
     """Main function"""
+
+    override_where()
 
     gph = GraphState(graph=[], wall_nodes=set())
 
@@ -26,21 +46,6 @@ def main() -> None:
     txt = VisText()
 
     run_pathfinding(gph, algo, lgc, txt)
-
-    # Fixes error when compiling program to exe.
-    if hasattr(sys, "frozen"):
-        import certifi.core
-
-        os.environ["REQUESTS_CA_BUNDLE"] = override_where()
-        certifi.core.where = override_where
-
-        # delay importing until after where() has been replaced
-        import requests.utils
-        import requests.adapters
-        # replace these variables in case these modules were
-        # imported before we replaced certifi.core.where
-        requests.utils.DEFAULT_CA_BUNDLE_PATH = override_where()
-        requests.adapters.DEFAULT_CA_BUNDLE_PATH = override_where()
 
 
 if __name__ == '__main__':
