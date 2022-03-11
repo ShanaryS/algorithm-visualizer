754e4138c3707e8d5e54f423306021141e3ace5d
Shanary Stewart <shanarys@gmail.com>
2022-03-11 00:21:42 -0500

    Added min time

--- src/timer.py
+++ src/timer.py
@@ -10,7 +10,11 @@ Usage:
 import time
 
 
-g_timer_start = g_timer_count = g_timer_max = g_timer_total_time = 0
+g_timer_start = 0
+g_timer_count = 0
+g_timer_min = float('inf')
+g_timer_max = float('-inf')
+g_timer_total_time = 0
 
 
 def timer_start() -> None:
@@ -23,20 +27,23 @@ def timer_end() -> None:
     """End timer for a section of code"""
     end = time.time_ns()
     
-    global g_timer_start, g_timer_count, g_timer_max, g_timer_total_time
+    global g_timer_start, g_timer_count, g_timer_min
+    global g_timer_max, g_timer_total_time
     g_timer_count += 1
     total = end - g_timer_start
+    g_timer_min = min(g_timer_min, total)
     g_timer_max = max(g_timer_max, total)
     g_timer_total_time += total
 
 
 def timer_print() -> None:
     """Prints info about the timer"""
-    global g_timer_start, g_timer_count, g_timer_max, g_timer_total_time
+    global g_timer_start, g_timer_count, g_timer_min
+    global g_timer_max, g_timer_total_time
     
     # Display total time with correct units
-    if g_timer_total_time > 10**9 * 60:
-        final_total = g_timer_total_time / 10**9 * 60
+    if g_timer_total_time > (10**9 * 60):
+        final_total = g_timer_total_time / (10**9 * 60)
         time_unit = "min"
     elif g_timer_total_time > 10**9:
         final_total = g_timer_total_time / 10**9
@@ -51,9 +58,26 @@ def timer_print() -> None:
         final_total = g_timer_total_time
         time_unit = "ns"
     
+    # Display min time with correct units
+    if g_timer_min > (10**9 * 60):
+        final_min = g_timer_min / (10**9 * 60)
+        time_unit = "min"
+    elif g_timer_min > 10**9:
+        final_min = g_timer_min / 10**9
+        time_unit = "s"
+    elif g_timer_min > 10**6:
+        final_min = g_timer_min / 10**6
+        time_unit = "ms"
+    elif g_timer_min > 10**3:
+        final_min = g_timer_min / 10**3
+        time_unit = "us"
+    else:
+        final_min = g_timer_min
+        time_unit = "ns"
+    
     # Display max time with correct units
-    if g_timer_max > 10**9 * 60:
-        final_max = g_timer_max / 10**9 * 60
+    if g_timer_max > (10**9 * 60):
+        final_max = g_timer_max / (10**9 * 60)
         time_unit = "min"
     elif g_timer_max > 10**9:
         final_max = g_timer_max / 10**9
@@ -70,8 +94,13 @@ def timer_print() -> None:
     
     print("-------------------------------------")
     print(f"Total Time: {final_total:.2f}{time_unit}")
+    print(f"Min Time: {final_min:.2f}{time_unit}")
     print(f"Max Time: {final_max:.2f}{time_unit}")
-    print(f"Count: {g_timer_count}")
+    print(f"Count: {g_timer_count:,}")
     print("-------------------------------------")
     
-    g_timer_start = g_timer_count = g_timer_max = g_timer_total_time = 0
+    g_timer_start = 0
+    g_timer_count = 0
+    g_timer_min = 0
+    g_timer_max = 0
+    g_timer_total_time = 0
