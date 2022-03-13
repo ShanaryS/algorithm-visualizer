28a6d13f94658b185d96db68d51d19fa837c7a0b
ShanaryS <shanarys@gmail.com>
2022-03-13 14:55:26 -0400

    Comments

--- run_pathfinding_visualizer.py
+++ run_pathfinding_visualizer.py
@@ -66,8 +66,6 @@ if __name__ == "__main__":
     #   - Key functions speed to check: update_neighbours, change_graph_size,
     #   set_squares_to_roads, 
     #   - node.py
-    #       - _discard_node
-    #       - clear_all_node_lists
     #       - update_nieghbours
     #       - _update_surrounding_neighbour_pool
     #   - graph.py
