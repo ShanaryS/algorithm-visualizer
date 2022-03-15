e941118fe4126d8c4d12c7ebe8e96f13926a38c2
ShanaryS <shanarys@gmail.com>
2022-03-15 15:57:22 -0400

    Comment

--- run_pathfinding_visualizer.py
+++ run_pathfinding_visualizer.py
@@ -51,6 +51,7 @@ if __name__ == "__main__":
     main()
 
 # --- Mutltithreading/Multiprocessing ---
+# Update algo after removing a node, maybe put current handling from left click in function?
 # Maybe just put pygame and logic on two distinct processes at leave it at that
 #   - Else probably would need to mantually set processes then manually assign it to everything
 #   - Need to already have processes started else there won't be any performance increase
