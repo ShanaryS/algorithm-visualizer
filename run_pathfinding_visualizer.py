03501c2b8965cee4349f5d722aed817b69809335
ShanaryS <shanarys@gmail.com>
2022-03-21 11:31:19 -0400

    Comment

--- run_pathfinding_visualizer.py
+++ run_pathfinding_visualizer.py
@@ -51,7 +51,6 @@ if __name__ == "__main__":
     main()
 
 # --- Mutltithreading/Multiprocessing ---
-# Move draw_vis_text into draw function and inherit it's params
 # Update algo after removing a node, maybe put current handling from left click in function?
 # Maybe just put pygame and logic on two distinct processes at leave it at that
 #   - Else probably would need to mantually set processes then manually assign it to everything
