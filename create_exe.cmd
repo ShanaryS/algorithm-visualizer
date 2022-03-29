4618cea1dc3d570f7a214d97b6a976ec6e65fb91
Shanary Stewart <shanarys@gmail.com>
2022-03-29 13:37:59 -0400

    Update

--- create_exe.cmd
+++ create_exe.cmd
@@ -5,7 +5,7 @@ rmdir /s /q "bin"
 set name="Pathfinding Visualizer"
 set src="run_pathfinding_visualizer.py"
 
-venv\Scripts\pyinstaller.exe -F -n %name% -p "venv\Lib\site-packages" -p "src\py" --noconsole --distpath ./bin %src%
+venv\Scripts\pyinstaller.exe -F -n %name% -p "venv\Lib\site-packages" -p "src\pathfinding" --noconsole --distpath ./bin %src%
 signtool sign /a bin\\%name%.exe
 signtool timestamp /t http://timestamp.digicert.com bin\\%name%.exe
 
@@ -19,7 +19,7 @@ copy "venv\Lib\site-packages\certifi\cacert.pem" "bin\lib"
 set name="Sorting Visualizer"
 set src="run_sorting_visualizer.py"
 
-venv\Scripts\pyinstaller.exe -F -n %name% -p "venv\Lib\site-packages" -p "src\py" --noconsole --distpath ./bin %src%
+venv\Scripts\pyinstaller.exe -F -n %name% -p "venv\Lib\site-packages" -p "src\sorting" --noconsole --distpath ./bin %src%
 signtool sign /a bin\\%name%.exe
 signtool timestamp /t http://timestamp.digicert.com bin\\%name%.exe
 
@@ -30,7 +30,7 @@ rmdir /s /q "__pycache__" "build"
 set name="Searching Visualizer"
 set src="run_searching_visualizer.py"
 
-venv\Scripts\pyinstaller.exe -F -n %name% -p "venv\Lib\site-packages" -p "src\py" --noconsole --distpath ./bin %src%
+venv\Scripts\pyinstaller.exe -F -n %name% -p "venv\Lib\site-packages" -p "src\searching" --noconsole --distpath ./bin %src%
 signtool sign /a bin\\%name%.exe
 signtool timestamp /t http://timestamp.digicert.com bin\\%name%.exe
 
