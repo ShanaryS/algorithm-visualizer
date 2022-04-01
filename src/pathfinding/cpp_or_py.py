e274cc4762791138f07b1aed61130aec976f871a
ShanaryS <shanarys@gmail.com>
2022-04-01 09:14:42 -0400

    Control cpp includes from json file

--- src/pathfinding/cpp_or_py.py
+++ src/pathfinding/cpp_or_py.py
@@ -1,4 +1,4 @@
-"""Use python or c++ implementation for certain parts of code.
+"""Use python or C++ implementation for certain parts of code.
 For pure python, choose ".py". For maximum C++ usage, choose highest item.
 Going up the import tree will override those python implementations with C++.
 Items higher up the import tree requires those below.
@@ -6,22 +6,42 @@ Example: "algorithms.h" will also include "square.h" automatically.
 C++ imports are optional but program will always depend on python.
 """
 
-######################################################################
-_include = "square.h"  # <----- ONLY CHANGE THIS!!! #
-######################################################################
-""" --- Import Tree: Choose One ---
-    "algorithms.h"
-    "square.h"
-    ".py"
-"""
+
+import json
+
+
+json_name = "include_cpp.json"
+
+try:
+    with open(json_name, encoding='utf-8') as json_file:
+        _use_cpp = json.load(json_file)
+except (FileNotFoundError, json.JSONDecodeError):
+    with open(json_name, 'w', encoding='utf-8') as json_file:
+        _use_cpp = {
+            "#include": ".py",
+            "How to Use": [
+                "Use python or C++ implementation for certain parts of code",
+                "For pure python, choose '.py'. For maximum C++ usage, choose highest item.",
+                "Going up the import tree will override those python implementations with C++.",
+                "Items higher up the import tree requires those below.",
+                "Example: 'algorithms.h' will also include 'square.h' automatically.",
+                "C++ imports are optional but the program will always depend on python.",
+                "######################################################################",
+                "--- Import Tree: #include only one ---",
+                "algorithms.h",
+                "square.h",
+                ".py"
+            ]
+        }
+        json.dump(_use_cpp, json_file, indent=4)
 
 # Setup imports across program.
 use_square_h = False
 use_algorithms_h = False
-if _include == ".py":
+if _use_cpp["#include"] == ".py":
     pass
-elif _include == "square.h":
+elif _use_cpp["#include"] == "square.h":
     use_square_h = True
-elif _include == "algorithms.h":
+elif _use_cpp["#include"] == "algorithms.h":
     use_square_h = True
     use_algorithms_h = True
